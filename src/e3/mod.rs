65c13eb8ae6d6d0dc6e3c44b1565d5808e5fe55c
Andrew Koroluk <koroluka@gmail.com>
2016-09-01 03:32:49 -0400

    add e3

--- src/e3/mod.rs
+++ src/e3/mod.rs
@@ -1,3 +1,25 @@
+fn is_prime(n: u32) -> bool {
+	let mut i = 2;
+	//let sqrt_n = sqrt(n).tofixed(0);
+	let sqrt_n = 4;
+
+	while i <= sqrt_n {
+		if n % i == 0 {
+			return false;
+		} else {
+			i += 1;
+		}
+	}
+
+	true
+}
+
+const NUM: u64 = 600851475143;
+
 pub fn main() -> u32 {
+	println!("{}", is_prime(13));
+
+	println!("{}", NUM);
+
     0
 }

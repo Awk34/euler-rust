4c1d55c6bbb9401408f4bde5eb07dc825b3fc081
Andrew Koroluk <andrew.koroluk@datavoke.com>
2017-05-15 15:01:37 -0400

    feat(main): show compute duration

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,36 @@
 //use std::io;
 extern crate euler;
+extern crate time;
 
 use euler::problems::problem_001;
+use time::PreciseTime;
 
 fn main() {
+    let start = PreciseTime::now();
+
     println!("E1: {}", problem_001::main());
+
+    let end = PreciseTime::now();
+
+    let duration = start.to(end);
+    let seconds = duration.num_seconds();
+    let milliseconds = duration.num_milliseconds();
+    let microseconds = duration.num_microseconds().unwrap();
+    let nanoseconds = duration.num_nanoseconds().unwrap();
+
+    if seconds < 1 as i64 {
+        if milliseconds < 1 as i64 {
+            if microseconds < 1 as i64 {
+                println!("Time: {} nanoseconds", nanoseconds);
+            } else {
+                println!("Time: {} microseconds", microseconds);
+            }
+        } else {
+            println!("Time: {} milliseconds", milliseconds);
+        }
+    } else {
+        println!("Time: {} seconds", seconds);
+    }
 
     // let mut number = String::new();
 
